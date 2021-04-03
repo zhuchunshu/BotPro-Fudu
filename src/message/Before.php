bd8a3bccda272a6c4d7eb27ae4d6ce51d7ca792d
Inkedus <inkedus@InkedusdeMacBook-Pro.local>
2021-04-03 16:11:18 +0800

    禁言群成员

--- src/message/Before.php
+++ src/message/Before.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Plugins\fudu\src\message;
 
 use App\Plugins\fudu\src\Fudu;
@@ -6,7 +7,8 @@ use App\Plugins\fudu\src\Fudu;
 /**
  * 后指令
  */
-class Before{
+class Before
+{
 
     /**
      * 接收到的数据
@@ -36,23 +38,63 @@ class Before{
      */
     public $PluginData;
 
-    public function __construct($data,$order,$orderCount,$PluginData)
+    public function __construct($data, $order, $orderCount, $PluginData)
     {
         $this->data = $data;
         $this->order = $order;
         $this->orderCount = $orderCount;
         $this->PluginData = $PluginData;
     }
-    public function 链接分享(){
-        if($this->orderCount>=3){
+    public function 链接分享()
+    {
+        if ($this->orderCount >= 3) {
             sendMsg([
                 'group_id' => $this->data->group_id,
                 'message' => "[CQ:share,url={$this->order[1]},title={$this->order[2]}]"
             ], "send_group_msg");
-        }else{
+        } else {
             sendMsg([
                 'group_id' => $this->data->group_id,
                 'message' => "条件不满足"
+            ], "send_group_msg");
+        }
+    }
+    public function 禁言()
+    {
+        if ($this->orderCount >= 4) {
+            $duration = $this->order[2];
+            if (is_numeric($duration)) {
+                switch ($this->order[3]) {
+                    case '分钟':
+                        $duration = $duration*60;
+                        break;
+                    case '小时':
+                        $duration = $duration*60*60;
+                        break;
+                    case '天':
+                        $duration = $duration*60*60*24;
+                        break;
+                    default:
+                        $duration = $duration*1;
+                        break;
+                }
+                sendMsg([
+                    'group_id' => $this->data->group_id,
+                    'user_id' => cq_at_qq($this->data->message),
+                    'duration' => $duration
+                ], "set_group_ban");
+            } else {
+                sendMsg([
+                    'group_id' => $this->data->group_id,
+                    'message' => "禁言时长必须是数字"
+                ], "send_group_msg");
+            }
+        } else {
+            sendMsg([
+                'group_id' => $this->data->group_id,
+                'message' => "条件不满足，用法:
+禁言 @被禁言的人 时长 时间格式
+禁言 @张三 60 秒(分钟、小时、天)"
             ], "send_group_msg");
         }
     }
